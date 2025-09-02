3d80ca4eb7aff9ba2944bb00502f0b3dda843ec6
ram <ram@maheshwariram.com>
2025-09-02 15:15:17 +0200

    build: added tailwind

--- svelte-app/src/routes/+layout.svelte
+++ svelte-app/src/routes/+layout.svelte
@@ -1,12 +1,12 @@
-<script lang="ts">
-    import favicon from '$lib/assets/favicon.svg';
-    import '../app.css';
+<script>
+	import favicon from '$lib/assets/favicon.svg';
+	import '../app.css';
 </script>
 
 <svelte:head>
-    <link rel="icon" href={favicon} />
+	<link rel="icon" href={favicon} />
 </svelte:head>
 
-<div class="bg-white text-gray-800 min-h-screen font-sans">
-    <slot />
+<div class="min-h-screen bg-white font-sans text-gray-800">
+	<slot></slot>
 </div>
