addcac91ab85fe99cfad5b78f718321962fef13b
ram <ram@maheshwariram.com>
2025-09-02 17:08:21 +0200

    fix(ci): remove GH actions workflow, try to deploy this on vercel

--- svelte-app/svelte.config.js
+++ svelte-app/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -6,17 +6,9 @@ const config = {
     preprocess: vitePreprocess(),
 
     kit: {
-        adapter: adapter({
-            pages: 'build',
-            assets: 'build',
-            fallback: 'index.html',
-            precompress: false,
-            strict: true
-        }),
-        paths: {
-            base: process.env.NODE_ENV === 'production' ? '/AlbumPosterGenerator' : ''
-        }
+        adapter: adapter()
     }
 };
 
 export default config;
+
